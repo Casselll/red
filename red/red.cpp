b85636dea0a52a35a830f202b2d50fdbbd347fe1
yz18183 <yz18183@essex.ac.uk>
2020-11-25 16:39:31 +0800

    添加项目文件。

---
+++ red/red.cpp
@@ -0,0 +1,27 @@
+// red.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<cstdlib>
+#include<opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+using namespace cv;3
+
+
+vector<Mat> input(string path)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
+
